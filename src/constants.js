93c96ba1883d962285f0a421305776c250e07b4f
DarkVortex <112894912+Jaun-van-Deventer@users.noreply.github.com>
2024-11-13 20:40:44 +0200

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `<p><strong>Excuse the computer it runs a bit slow but it gets there eventually</strong></p>
+        <div style="border: 2px solid black; padding: 10px;">
+            <a href="linkedin" target="_blank" title="LinkedIn"><i class="fa-brands fa-linkedin fa-4x" style="color: #005799;"></i></a>
+            <a href="https://github.com" target="_blank" title="GitHub"><i class="fa-brands fa-github fa-4x" style="color: #1f2f4d;"></i></a>
+        </div>`,
+    "cs-degree": `<img src="/resume_game/cert.png" alt="CS5-: Introduction to Computer Science" loading="lazy" />`,
+    "sofa-table":`Let's sit down and have a chat, you can contact me at <strong>+27 79 741 6919</strong> or <strong>jaunvdeventer78@gmail.com</strong>`,
+    tv: `<iframe width="1500" height="748" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    bed: `This is the Bed, where I hardly get any sleep`,
+    resume: `<img src="/resume_game/res.png" alt="My Resume" loading="lazy" width:"800"/>`,
+    projects: `
+                <a
+                    href="https://github.com/Jaun-van-Deventer/Quicklink-QR"
+                    >Quicklink-QR Chrome Extension</a>
+                <a
+                    href="https://jaun-van-deventer.github.io/react-movie-database/"
+                    >Movie Browser </a>
+                <a
+                    href="https://github.com/Jaun-van-Deventer/inventory-management"
+                    >Inventory Management app (Work in progress)</a><br>
+                <a
+                    href="/src/projects/Pig-Game"
+                    >Pig Game</a>
+                <a
+                    href="/src/projects/Guess-My-Number"
+                    >Guess my Number</a>`,
+    library: `<p>
+                These are the technologies I have previously used:
+                <strong>JavaScript</strong>, <strong>TypeScript</strong>, <strong>C</strong>, <strong>C#</strong>, 
+                <strong>Docker</strong>, <strong>MongoDB</strong>, <strong>SQL</strong>, <strong>Git</strong>, 
+                <strong>React</strong>, <strong>Node.js</strong>, <strong>Python</strong>, <strong>Flask</strong>, 
+                <strong>Bootstrap</strong>, <strong>CSS</strong>, <strong>HTML</strong>
+            </p>`
+    ,exit: `Thank you for visiting my resume, to leave you can simply close the tab`,
+}
+
